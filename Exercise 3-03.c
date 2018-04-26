1262e50c61b34ce32f14cfe45be44f32822c43c4
Borntuft <32026074+borntuft@users.noreply.github.com>
2018-04-25 20:20:25 -0400

    Update Exercise 3-03.c

--- Exercise 3-03.c
+++ Exercise 3-03.c
@@ -1,6 +1,6 @@
 /*
 
-Exercise 3-3. Write a function expand (s 1, s2) that expands shorthand nota-
+Exercise 3-3. Write a function expand (s1, s2) that expands shorthand nota-
 tions like a - z in the string s1 into the equivalent complete list abc ... xyz in
 s2. Allow for letters of either case and digits, and be prepared to handle cases
 like a-b-c and a-zO-9 and -a-z. Arrange that a leading or trailing - is
@@ -45,19 +45,20 @@ void getLine (char s1[])
 
 void expand (char s1[], char s2[])
 {
+    int counter = 0;
     int i = 0;
     int j = 0;
     int startCharacter = 0;
     int endCharacter = 0;
 
-    while (s1[i] != '\0')
+    while ((s1[i] != '\0'))
     {
         s2[j++] = s1[i++];
         if ((s1[i] == '-') && (isalnum(s1[i + 1])) && (isalnum(s1[i - 1])))
         {
             startCharacter = s1[i - 1];
             endCharacter   = s1[++i];
-            while (startCharacter < endCharacter - 1)
+            while ((counter++ < LIMIT - 1) && (startCharacter < endCharacter - 1))
                 s2[j++] = ++startCharacter;
         }
     }
